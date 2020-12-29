6049f9dd8d7b761a23d14007d4301cd3762ca882
NIMOWAIRIMU <nimowairimu25@gmail.com>
2020-12-29 18:07:12 +0300

    make changes to portfolio.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
+    $("#hide-design").click(function(){
+        $(".img1").toggle(100);
+        $(".hide").toggle(100);
+    });
 
-  $("#design").click(function(){
-    $(".hide-image").toggle();
-    $("#design-show").toggle();
-  })
+  $('.submit').click(function () {
+          var Name = $('#name').val();
+          var Email = $('#email').val();
+          var Message = $('#message');
+          var key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
+          if (Name == '' || Email == '' || Message == '') {
+              alert('Please make sure you have filled in the form correctly!');
+          } else {
+              alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
+          }
+    });
