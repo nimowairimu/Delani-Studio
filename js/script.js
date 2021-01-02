0d997cd9c6a961d53a38a5b4021543d3eb1c215b
nimowairimu <nimowairimu25@gmail.com>
2021-01-03 01:12:30 +0300

    Add bounce animation on mouse.

--- js/script.js
+++ js/script.js
@@ -1,17 +1,74 @@
-$(document).ready(function() {
-    $("#hide-design").click(function(){
-        $(".img1").toggle(100);
-        $(".hide").toggle(100);
+$(function () {
+    let desp = $('#desp');
+    let devp = $('#devp');
+    let prop = $('#prop');
+    let destext = $('#destxt');
+    let devtext = $('#devtext');
+    let protext = $('#protext');
+
+    $(desp).hide();
+    $(devp).hide();
+    $(prop).hide();
+
+    //Show paragraph content
+    $('.des').on('click', function () {
+        $('.des').slideUp('slow');
+        $(desp).show(500);
+        $(destext).animate({fontWeight:'bolder',fontSize:'30px'})
+
+    });
+    $('.dev').on('click', function () {
+        $('.dev').slideUp('slow');
+        $(devp).show(500);
+        $(devtext).animate({fontWeight:'bolder',fontSize:'30px'})
+    });
+    $('.pro').on('click', function () {
+        $('.pro').slideUp('slow');
+        $(prop).show(500);
+        $(protext).animate({fontWeight:'bolder',fontSize:'30px'})
     });
 
-  $('.submit').click(function () {
-          var Name = $('#name').val();
-          var Email = $('#email').val();
-          var Message = $('#message');
-          var key ='c8fbd9a0cf3958e02be2822e55cfbfc5-us4';
-          if (Name == '' || Email == '' || Message == '') {
-              alert('Please make sure you have filled in the form correctly!');
-          } else {
-              alert(' Hi ' + Name + ' We have received your message. Thank you for reaching out to us.');
-          }
+    //Hide paragraph content
+    $(desp, destext).on('click', function () {
+        $('.des').slideDown();
+        $(desp).hide(500);
+        $(destext).animate({fontWeight:'normal',fontSize:'16px'})
     });
+    $(devp, devtext).on('click', function () {
+        $('.dev').slideDown();
+        $(devp).hide(500);
+        $(devtext).animate({fontWeight:'normal',fontSize:'16px'})
+    });
+    $(prop, protext).on('click', function () {
+        $('.pro').slideDown();
+        $(prop).hide(500);
+        $(protext).animate({fontWeight:'normal',fontSize:'16px'})
+    })
+
+    //Overlay images
+
+    $('.portimg').hover(function(){
+        $('.porttext',this).slideToggle('slow');
+     }, function(){
+        $('.porttext',this).slideToggle('slow');
+     });
+
+
+    //Form Validation
+    $("form#form").on('submit',function(event){
+           event.preventDefault();
+           let name = $("input#name").val();
+           $('#form').ajaxChimp({
+               url: 'https://gmail.us7.list-manage.com/subscribe/post?u=7aa1547296855bada1640d850&amp;id=ae514885e7',
+               callback: callbackFunction
+           });
+
+           function callbackFunction (resp) {
+               console.log(resp);
+               if (resp.result === 'success') {
+                   alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you shortly...cheers");
+                }else{
+                    alert("Please provide your correct name and email!");
+                }
+            }
+        });
