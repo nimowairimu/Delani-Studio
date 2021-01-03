f50153dcbbd9462ffa901e2368da2b3dffe9c20d
nimowairimu <nimowairimu25@gmail.com>
2021-01-03 08:40:28 +0300

    Add icons to what we do section.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,26 @@
 $(function () {
-    let desp = $('#desp');
-    let devp = $('#devp');
+    let dtxt = $('#dtxt');
+    let devtxt = $('#devtxt');
     let prop = $('#prop');
-    let destext = $('#destxt');
-    let devtext = $('#devtext');
+    let dicon = $('#dicon');
+    let devicon = $('#devicon');
     let protext = $('#protext');
 
-    $(desp).hide();
-    $(devp).hide();
+    $(dtxt).hide();
+    $(devtxt).hide();
     $(prop).hide();
 
     //Show paragraph content
     $('.des').on('click', function () {
         $('.des').slideUp('slow');
-        $(desp).show(500);
-        $(destext).animate({fontWeight:'bolder',fontSize:'30px'})
+        $(dtxt).show(500);
+        $(dicon).animate({fontWeight:'bolder',fontSize:'30px'})
 
     });
     $('.dev').on('click', function () {
         $('.dev').slideUp('slow');
         $(devp).show(500);
-        $(devtext).animate({fontWeight:'bolder',fontSize:'30px'})
+        $(devicon).animate({fontWeight:'bolder',fontSize:'30px'})
     });
     $('.pro').on('click', function () {
         $('.pro').slideUp('slow');
@@ -56,19 +56,23 @@ $(function () {
 
     //Form Validation
     $("form#form").on('submit',function(event){
-           event.preventDefault();
-           let name = $("input#name").val();
-           $('#form').ajaxChimp({
-               url: 'https://gmail.us7.list-manage.com/subscribe/post?u=7aa1547296855bada1640d850&amp;id=ae514885e7',
-               callback: callbackFunction
-           });
+         event.preventDefault();
+         let name = $("input#name").val();
+         $('#form').ajaxChimp({
+             url: 'https://gmail.us7.list-manage.com/subscribe/post?u=7aa1547296855bada1640d850&amp;id=ae514885e7',
+             callback: callbackFunction
+         });
 
-           function callbackFunction (resp) {
-               console.log(resp);
-               if (resp.result === 'success') {
-                   alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you shortly...cheers");
-                }else{
-                    alert("Please provide your correct name and email!");
-                }
-            }
-        });
+         function callbackFunction (resp) {
+             console.log(resp);
+             if (resp.result === 'success') {
+                 alert ("Hey " + name + ", Thanks for reaching out, we'll get back to you shortly...cheers");
+              }else{
+                  alert("Please provide your correct name and email!");
+              }
+          }
+      });
+
+
+
+});
